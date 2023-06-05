d898abd8cc02ec3f49f02e96838ea2a1dd962958
David <david656558a@gmail.com>
2023-06-05 17:34:32 +0400

    changed functonality upload photo form data to base64

--- app/Helper/UploadFile.php
+++ app/Helper/UploadFile.php
@@ -4,6 +4,9 @@
 namespace App\Helper;
 
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
 class UploadFile
 {
 
@@ -29,4 +32,42 @@ class UploadFile
 
         return $filePath;
     }
+
+    public static function base64($base64fileString, $typeFile = 'image')
+    {
+        if (!$base64fileString) {
+            return null;
+        }
+        $random = Str::random(40);
+        $f = finfo_open();
+
+        $fileKay = 1;
+        if ($typeFile === 'image'){
+            $mime_type = finfo_buffer($f, base64_decode(explode(',',$base64fileString)[1]), FILEINFO_MIME_TYPE);
+        }elseif ($typeFile === 'zip'){
+            $mime_type = finfo_buffer($f, base64_decode($base64fileString), FILEINFO_MIME_TYPE);
+        }elseif ($typeFile === 'pdf'){
+            $fileKay = 0;
+            $mime_type = finfo_buffer($f, base64_decode(explode(',',$base64fileString)[$fileKay]), FILEINFO_MIME_TYPE);
+        }
+
+        $type = explode('/',$mime_type);
+
+        if (!is_dir(storage_path("/app/public/repair/order"))) {
+            if(!is_dir(storage_path("/app/public/repair"))) {
+                File::makeDirectory(storage_path("app/public/repair"));
+            }
+            File::makeDirectory(storage_path("app/public/repair/order"));
+        }
+
+        file_put_contents(storage_path('app/public/repair/order/'.$random.'.'.$type[1]),print_r(base64_decode(explode(',',$base64fileString)[$fileKay]),true));
+        $req = 'storage/repair/order/'.$random.'.'.$type[1];
+
+        if (!$req) {
+            return null;
+        }
+
+        return $req;
+    }
+
 }
